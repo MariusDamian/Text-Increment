89ba28c53af08211a95c032988699d33df3c394a
Marius Damian <86361141+MariusDamian@users.noreply.github.com>
2021-09-25 16:31:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ let inputd = document.getElementById("inputd");
 let btnd = document.getElementById("btnd");
 
 function adauga() {
-     pd.innerHTML = "" + inputd.value;
-     inputd.value = "";
-}
-
-function adauga2() {
      if (inputd.value === "") {
           alert("Please insert text");
      } else {
